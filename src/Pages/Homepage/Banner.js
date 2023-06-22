3e353e27738a17ffe446701d6e464bdc59cb1662
tachyontechs <hafizabdullahqurashi1@gmail.com>
2023-06-22 18:25:43 +0500

    update

--- src/Pages/Homepage/Banner.js
+++ src/Pages/Homepage/Banner.js
@@ -1,11 +1,29 @@
-import React from "react";
+import React from 'react'
+import '../../Components/Scss/Home/Banner.scss'
 
 const Banner = () => {
   return (
-    <div className="flex flex-col  justify-center px-16 md:flex-row bg-[#212529] ">
-    
-    </div>
-  );
-};
+    <div>
 
-export default Banner;
+      <div className='banner-parent-back'>
+        <div className='banner-grid'>
+          <div className='person'>
+            <img src="./images/man.png" alt="" style={{ marginTop: '25px' }} />
+          </div>
+          <div className='search'>
+            <div className='cunty'>
+              <span>Find anythings in</span>
+              <button>Nigeria</button>
+            </div>
+            <input type="text" placeholder='I am looking for...' />
+          </div>
+          <div className='person'>
+            <img src='./images/girls.png' alt="" style={{ marginTop: '60px', width: '130%' }} />
+          </div>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default Banner
