297439f64a455c6a9e28131d38ef127f26e9e5b7
tachyontechs <hafizabdullahqurashi1@gmail.com>
2023-07-05 23:47:32 +0500

    update

--- src/Components/Forms/Form.js
+++ src/Components/Forms/Form.js
@@ -335,20 +335,7 @@ function Form() {
                                     </div>
 
 
-                                    <Box sx={{ width: '100%', typography: 'body1' }}>
-      <TabContext value={value}>
-        <Box sx={{ borderBottom: 1, borderColor: 'divider' }}>
-          <TabList onChange={handleChangese} aria-label="lab API tabs example">
-            <Tab label="Item One" value="1" />
-            <Tab label="Item Two" value="2" />
-            <Tab label="Item Three" value="3" />
-          </TabList>
-        </Box>
-        <TabPanel value="1">Item One</TabPanel>
-        <TabPanel value="2">Item Two</TabPanel>
-        <TabPanel value="3">Item Three</TabPanel>
-      </TabContext>
-    </Box>
+   
 
 
 
